5c9aa8280ac9532cdb7042b497ed236a3cbb9336
Murilo <mbalves1@outlook.com>
2024-07-19 18:36:55 -0300

    [feat]: Added @nestjs/mapped-types and moment-timezone dependencies and modified src/main.ts to use moment-timezone for date formatting

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   // app.useGlobalPipes(new ValidationPipe());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
   const PORT = 8080;
   await app.listen(PORT);
 }
